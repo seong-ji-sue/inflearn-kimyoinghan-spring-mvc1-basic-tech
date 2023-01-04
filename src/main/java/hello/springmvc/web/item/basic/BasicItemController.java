36dcb011d1872c9b68103803ce0e72ffec76c0d8
jisue <tjdwltn121@gmail.com>
2023-01-04 15:58:39 +0900

    상품 상세 폼 매핑

--- src/main/java/hello/springmvc/web/item/basic/BasicItemController.java
+++ src/main/java/hello/springmvc/web/item/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -22,6 +23,14 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "html/items";
+    }
+
+    //상품 상세 폼
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "html/item";
     }
 
     @PostConstruct
