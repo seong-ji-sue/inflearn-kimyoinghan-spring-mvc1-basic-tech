624c198d4167236c36166c1f26160213e44c6a2c
jisue <tjdwltn121@gmail.com>
2023-01-04 15:33:11 +0900

    상품 리스트 폼 생성 및 컨트롤러 만들기

---
+++ src/main/java/hello/springmvc/web/item/basic/BasicItemController.java
@@ -0,0 +1,25 @@
+package hello.springmvc.web.item.basic;
+
+import hello.springmvc.domain.item.Item;
+import hello.springmvc.domain.item.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor //생성자 자동 주입
+public class BasicItemController {
+    private final ItemRepository itemRepository; //자동주입
+
+    @GetMapping
+    public String items(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "html/items";
+    }
+}
